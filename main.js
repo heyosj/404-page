bf7f982b7851def48da18367b0da60f93308eaa9
OJ Sanchez <ojsanch@gmail.com>
2020-03-04 17:37:40 -0600

    dark mode working

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-let nav = document.querySelector(".hamburger");
-let navLinks = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+const sun = document.querySelector(".sun");
+const body = document.querySelector("body");
+const homeButton = document.querySelector(".home");
+const bars = document.querySelectorAll(".bar");
+const imageID = document.getElementById("imageID");
 
-function hide(x) {
-  if (nav.classList.toggle("change")) {
-    navLinks.style.visibility = "visible";
-  } else {
-    navLinks.style.visibility = "hidden";
-  }
+function initiateDarkMode() {
+  body.classList.toggle("moon");
+  homeButton.style.color = "white";
+  bars.forEach(bar => (bar.style.backgroundColor = "white"));
+  imageID.src = "./imgs/moon.svg";
 }
+
+function initiateLightMode() {
+  imageID.src = "./imgs/sun.svg";
+  homeButton.style.color = "black";
+  bars.forEach(bar => (bar.style.backgroundColor = "black"));
+}
+
+sun.addEventListener("click", () => {
+  initiateDarkMode();
+
+  if (body.className != "moon") {
+    initiateLightMode();
+  }
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("change");
+  navbar.classList.toggle("active");
+});
